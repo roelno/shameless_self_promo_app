da7d37a3093f9550363227d352dbf73c34465322
Ro <lichtseeker@gmail.com>
2023-08-25 19:10:48 -0700

    Add binding for activity_preview

--- app/src/main/java/com/zeldaselfpromoapp/PreviewActivity.kt
+++ app/src/main/java/com/zeldaselfpromoapp/PreviewActivity.kt
@@ -2,10 +2,18 @@ package com.zeldaselfpromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zeldaselfpromoapp.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPreviewBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewMessage.text = "I am manually setting some text in TextView."
     }
+
 }
